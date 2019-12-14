f56c5d7ab738d10f34d165be5111e3eb62e0a669
Andrew <andrewfrommoon@gmail.com>
2019-12-14 11:55:04 +0300

    Create pyx func

--- python/cmatch.pyx
+++ python/cmatch.pyx
@@ -47,14 +47,52 @@ def distance_rate(occurence):
     return sum(data)
 
 
+
+
+cdef float _get_weight(char* line, char* pattern, int line_len, int pattern_len):
+    cdef char line_char, pattern_char, first_pattern_char = pattern[0]
+    cdef int pattern_index, line_start, line_index, last_found_index
+    cdef float weight, max_weight = 0
+    for line_start in range(line_len):
+        pattern_index = 0
+        weight = 0
+        last_found_index = -1
+        pattern_char = pattern[pattern_index]
+        if line[line_start] != first_pattern_char:
+            continue
+        for line_index in range(line_start, line_len):
+            line_char = line[line_index]
+            if line_char == pattern_char:
+                if last_found_index != -1:
+                    weight += 1.0 / (line_index - last_found_index)
+                last_found_index = line_index
+                pattern_index += 1
+                if pattern_index == pattern_len:
+                    max_weight = max(weight, max_weight)
+                    break
+                pattern_char = pattern[pattern_index]
+        if weight == 0:
+            break
+    return max_weight
+
+
 cpdef get_weight(line, pattern):
+    line_len = len(line)
+    pattern_len = len(pattern)
     line = line.encode('UTF-8')
     pattern = pattern.encode('UTF-8')
-    occurences = find_occurences(line, pattern)
-    return max([distance_rate(occurence) for occurence in occurences])
+    return _get_weight(line, pattern, line_len, pattern_len)
 
 cpdef main():
-    line = "pewDiePie"
+    # D = 68
+    # P = 80
+    # e = 101
+    # i = 105
+    # p = 112
+    # w = 119
+    line = "pewDiePiepDP"
+    # line = "ppDP"
     pattern = "pDP"
+    pattern = ""
     w = get_weight(line, pattern)
     print("HELLO", w)
--- python/script.sh
+++ python/script.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 python setup.py build_ext -i
 python -c 'import cmatch; Q = cmatch.main()'
-# python matcher.py
+python matcher.py
